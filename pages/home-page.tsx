8686b2db9f1054aa13b9975bb38a7345af918bf2
ThangNguyen <nthellious@nthellious-MacBook-Air.local>
2022-09-13 20:02:38 +0700

    2 pages

---
+++ pages/home-page.tsx
@@ -0,0 +1,108 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+import home_styles from "../styles/Home.module.css";
+import common_styles from "../styles/Common.module.css";
+
+const HomePage: NextPage = () => {
+  return (
+    // <div className={`${common_styles.bg_main}`}>
+    <div
+      className={`${common_styles.bg_main} ${home_styles.home_page_container}`}
+    >
+      <div
+        className={`${common_styles.space_top}  ${common_styles.container} ${common_styles.full_screen_height}`}
+      >
+        <p className={`${home_styles.build_on_near}`}>
+          <Image
+            src={"/images/build-on-near.png"}
+            width={619}
+            height={187.32}
+            alt="Nearlend Dao build on Near"
+          />
+        </p>
+        <button
+          className={`${common_styles.btn_custom} ${home_styles.btn_launch_app}`}
+        >
+          Launch App
+        </button>
+
+        <p className={`${home_styles.main_img_big}`}>
+          <Image
+            src={"/images/main-img-big.png"}
+            width={450}
+            height={450}
+            alt="NearlendDao"
+          />
+        </p>
+
+        <p className={`${home_styles.deco_1}`}>
+          <Image
+            src={"/images/deco-1.png"}
+            width={250}
+            height={65}
+            alt="NearlendDao"
+          />
+        </p>
+
+        <p className={`${home_styles.deco_2}`}>
+          <Image
+            src={"/images/deco-2.png"}
+            width={250}
+            height={70}
+            alt="NearlendDao"
+          />
+        </p>
+
+        <p className={`${home_styles.deco_3}`}>
+          <Image
+            src={"/images/deco-3.png"}
+            width={280}
+            height={90}
+            alt="Nearlend Dao"
+          />
+        </p>
+
+        <p className={`${home_styles.logo_usn}`}>
+          <Image
+            src={"/images/main-logo-usn.png"}
+            width={57}
+            height={57}
+            alt="USN USDT"
+          />
+        </p>
+
+        <p className={`${home_styles.logo_near}`}>
+          <Image
+            src={"/images/main-logo-near.png"}
+            width={57}
+            height={57}
+            alt="USN USDT"
+          />
+        </p>
+
+        <p className={`${home_styles.logo_eth}`}>
+          <Image
+            src={"/images/main-img-blur-2.png"}
+            width={180}
+            height={180}
+            alt="Ethereum"
+          />
+        </p>
+
+        <p className={`${home_styles.logo_bitcoin}`}>
+          <Image
+            src={"/images/main-logo-bit-blur.png"}
+            width={240}
+            height={240}
+            alt="Bitcoin"
+          />
+        </p>
+      </div>
+    </div>
+    // </div>
+  );
+};
+
+export default HomePage;
---
+++ pages/portfolio.tsx
@@ -0,0 +1,149 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import React from "react";
+import portfolio_styles from "../styles/Portfolio.module.css";
+import common_styles from "../styles/Common.module.css";
+
+const Portfolio: NextPage = () => {
+  return (
+    <div className={`${common_styles.space_top} ${common_styles.bg_main}`}>
+      <div
+        className={`${common_styles.container} ${common_styles.full_screen_height}`}
+      >
+        <div className={`${portfolio_styles.welcome}`}>
+          <Image
+            src="/images/logo-nearlend-transparent.png"
+            layout="fixed"
+            width={64}
+            height={60}
+            alt="Logo Nearlend Dao"
+          />
+          <h3>Welcome to Nearlog</h3>
+          <button className={`${common_styles.btn_custom}`}>
+            Connect wallet
+          </button>
+        </div>
+        <div className={`${portfolio_styles.wrap_position}`}>
+          <div className={`${portfolio_styles.position}`}>
+            <div>
+              <h3>Open Positions</h3>
+              <p>You have no open positions</p>
+            </div>
+            <button className={`${common_styles.btn_custom}`}>
+              Start trading{" "}
+              <Image
+                src="/images/arrow-right.png"
+                layout="fixed"
+                width={13}
+                height={8}
+                alt="Arrow right"
+                style={{ paddingLeft: "2px" }}
+              />
+            </button>
+          </div>
+          <div
+            className={`${portfolio_styles.position} ${portfolio_styles.values}`}
+          >
+            <div>
+              <h3>Values</h3>
+            </div>
+            <div className={`${portfolio_styles.info_values}`}>
+              <div>
+                <h4>Total Value Locked</h4>
+                <p>$1.2m</p>
+              </div>
+              <div>
+                <h4>30D Volume</h4>
+                <p>$755k</p>
+              </div>
+              <div>
+                <h4>Open Interest</h4>
+                <p>$0.12m</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={`${portfolio_styles.wrap_market}`}>
+          <div
+            className={`${portfolio_styles.position} ${portfolio_styles.position_full_width}`}
+          >
+            <h3>Markets</h3>
+            <div className={`${common_styles.width_100_percent}`}>
+              <div className={`${portfolio_styles.wrap_market_title}`}>
+                <p>Market</p>
+                <p>24H Price</p>
+                <p>Open Interest</p>
+                <p>30D Volume</p>
+              </div>
+              {mock_data.map((item, idx) => {
+                return (
+                  <div
+                    key={item.token_name + idx}
+                    className={`${portfolio_styles.wrap_market_title}`}
+                  >
+                    <div className={`${portfolio_styles.market_info}`}>
+                      <div className={`${portfolio_styles.market_info_token}`}>
+                        <Image
+                          src={item.img}
+                          layout="fixed"
+                          width={32}
+                          height={32}
+                          alt={item.token_name}
+                        />
+                        <h6>{item.token_name}</h6>
+                      </div>
+                    </div>
+                    <div className={`${portfolio_styles.market_info}`}>
+                      <div className={`${portfolio_styles.market_info_value}`}>
+                        <h4>{item.price}</h4>
+                        <h6 className={`${portfolio_styles[`${item.status}`]}`}>
+                          {item.percent}
+                        </h6>
+                      </div>
+                    </div>
+                    <div className={`${portfolio_styles.market_info}`}>
+                      <div className={`${portfolio_styles.market_info_value}`}>
+                        <h4>{item.interest}</h4>
+                        <h6>{item.amount}</h6>
+                      </div>
+                    </div>
+                    <div className={`${portfolio_styles.market_info}`}>
+                      <div className={`${portfolio_styles.market_info_value}`}>
+                        <h4>{item.vol}</h4>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
+
+const mock_data = [
+  {
+    img: "/images/logo-near.png",
+    token_name: "Near",
+    price: "$5.3",
+    percent: "-0.513%",
+    interest: "$95,000.00",
+    amount: "29.0 NEAR",
+    vol: "$215,000.00",
+    status: "red",
+  },
+  {
+    img: "/images/logo-eth.png",
+    token_name: "Ethereum",
+    price: "$1,750.09",
+    percent: "+0.03378%",
+    interest: "$23,000.00",
+    amount: "5.41k ETH",
+    vol: "$215,000.00",
+    status: "green",
+  },
+];
